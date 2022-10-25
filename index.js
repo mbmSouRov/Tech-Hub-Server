33ff45c0ef7d7105fd4fb9666411ef400a636059
Murshid Bin Masud Sourov <61899629+mbmSouRov@users.noreply.github.com>
2022-10-25 19:50:44 +0600

    dynamic route created

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.get("/tech-categories", (req, res) => {
   res.send(categories);
 });
 
+app.get("/tech-categories/courseInfo/:id", (req, res) => {
+  const mew = req.params.id;
+
+  const courseInfo = categories.filter((x) => x.id === mew);
+  res.send(courseInfo);
+});
+
 app.listen(port, () => {
   console.log("Server is running now at port", port);
 });
